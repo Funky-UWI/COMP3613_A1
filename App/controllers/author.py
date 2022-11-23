33825d918653bab991748bf49970c9cf09e15f34
ShivsTKD <shivanmaharaj.sm@gmail.com>
2022-11-23 22:51:18 +0000

    Made changes to models to reflect class diagram Added comments to author controllers

--- App/controllers/author.py
+++ App/controllers/author.py
@@ -1,8 +1,8 @@
 from App.models import Author
 from App.database import db
 
-def create_author(first_name,last_name,email,password, qualifications): #qualifications
-    new_author = Author(first_name=first_name,last_name=last_name,email=email,password=password,qualifications= qualifications)
+def create_author(first_name, last_name, email, password):
+    new_author = Author(first_name=first_name, last_name=last_name, email=email, password=password)
     db.session.add(new_author)
     db.session.commit()
     return new_author
@@ -22,14 +22,16 @@ def get_all_authors_json():
 
 def get_author_by_name(name):
     print(name)
-    authors = Author.query.filter_by(name=name)
-    authors = [author for author in authors]
+    authors = Author.query.filter_by(first_name=name).all()
+    authors = list(authors)
     # this code should be in a different method
 
-    if not authors:
-        new_author = create_author(name=name, dob=None, qualifications=None)
-        authors = [new_author]
-        return authors
+    authors.append(list(Author.query.filter_by(last_name=name).all()))
+
+    # if not authors:                               This section needs to be reviewed, can't create author without 
+    #     new_author = create_author(name=name)     relevant parameters
+    #     authors = [new_author]
+    #     return authors
     return authors
 
 def create_new_author_account():
