e14d2a9ed74f733052e242842f000f45f1f2b17e
Trey Murray <treymurray13@outlook.com>
2022-11-24 17:15:18 +0000

    comment on publication controller

--- App/controllers/publication.py
+++ App/controllers/publication.py
@@ -2,6 +2,7 @@ from App.models import Publication
 from App.database import db
 
 def create_publication(title, authors, coauthors):
+    #Need to do a check on the coauthors before creating the publication to make default accounts for nonexistent ones.
     new_publication = Publication(title, authors, coauthors)
     db.session.add(new_publication)
     db.session.commit()
