0e9ac0f5cfc73418c6dbaf9585da261aef4c64ad
munesha55 <muneshabeharry@outlook.com>
2022-11-27 21:15:32 +0000

    Finished Add Publication and Add Authors pages

--- App/static/main.js
+++ App/static/main.js
@@ -1,22 +1,26 @@
+M.AutoInit();
 
-async function getUserData(){
-    const response = await fetch('/api/users');
-    return response.json();
-}
+let i = 1;
+function add_author(){
+    let form = document.getElementById("authors_form");
+    i += 1;
+    form.innerHTML += `
+        <div class="row">
+            <div class="col s6">${i}</div>
+            <div class="col s12 input-field">
+                <label for="fname">First Name</label>
+                <input class="input-field col s12" type="text" name="fname">
+            </div>
 
-function loadTable(users){
-    const table = document.querySelector('#result');
-    for(let user of users){
-        table.innerHTML += `<tr>
-            <td>${user.id}</td>
-            <td>${user.username}</td>
-        </tr>`;
-    }
-}
+            <div class="col s12 input-field">
+                <label for="lname">Last Name</label>
+                <input class="input-field col s12" type="text" name="lname">
+            </div>
 
-async function main(){
-    const users = await getUserData();
-    loadTable(users);
-}
-
-main();
+            <div class="col s12 input-field">
+                <label for="email">Email</label>
+                <input class="input-field col s12" type="text" name="email">
+            </div>
+        </div>
+    `;
+};
