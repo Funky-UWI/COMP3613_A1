62c8ffdcc479b7074fb4b83fe279a07dce74e9e7
Trey Murray <treymurray13@outlook.com>
2022-11-27 17:17:31 +0000

    fixed

--- App/controllers/publication.py
+++ App/controllers/publication.py
@@ -2,17 +2,18 @@ from App.models import Publication,PublishingRecord
 from App.database import db
 from . import author
 
-def create_publication(title, fields, publication_date, authors):
-    new_publication = Publication(title, fields, publication_date)
+def create_publication(title, field, publication_date, authors):
+    new_publication = Publication(title, field, publication_date)
     db.session.add(new_publication)
-    for author in authors:
-        exists = author.get_author_by_name(author.first_name,author.last_name)
+    db.session.commit()
+    for auth in authors:
+        exists = author.get_author_by_name(auth.first_name,auth.last_name)
         if exists == None:
-            new_author = author.create_default_author_account(author.first_name,author.last_name)
+            new_author = author.create_default_author_account(auth.first_name,auth.last_name)
             new_record = PublishingRecord(new_author.id, new_publication.id)
             db.session.add(new_record)
         else:
-            new_record = PublishingRecord(author.id, new_publication.id)
+            new_record = PublishingRecord(exists.id, new_publication.id)
             db.session.add(new_record)
     db.session.commit()
 
